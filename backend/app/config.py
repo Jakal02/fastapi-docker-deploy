bce426d6b086eae2e1305cec2f079d19fe484212
Jakal02 <gitcontribs@eligrubbs.com>
2024-03-15 13:19:42 -0400

    configured production DB connection to Cloud SQL instance.

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from sqlalchemy.engine.url import URL
+
 from pydantic import PositiveInt, PostgresDsn, ValidationInfo, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -14,7 +16,7 @@ class APISettings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="allow"
     )
 
-    NODE: str | None = "production"
+    NODE: str | None = "develop"
 
     POSTGRES_SERVER: str | None = None
     POSTGRES_SERVER_PORT: PositiveInt | None = 5432
@@ -30,6 +32,9 @@ class APISettings(BaseSettings):
     @classmethod
     def assemble_db_connection(cls, v: str | None, values: ValidationInfo) -> Any:
         """Create the database connection URI from .env variables."""
+        if values.data.get("NODE") == "production":
+            print("Creating SQLALCHEMY_DATABASE_URI from GCP Secrets...") # noqa T201
+            return cls._create_prod_db_url(cls, values)
         if isinstance(v, str):
             print("Loading SQLALCHEMY_DATABASE_URI from .docker.env file ...")  # noqa: T201
             return v
@@ -46,6 +51,31 @@ class APISettings(BaseSettings):
     def get_db_uri_string(self) -> str:
         """Return the string format of the database URI."""
         return self.SQLALCHEMY_DATABASE_URI.unicode_string()
+    
+    def _create_prod_db_url(self, values: ValidationInfo) -> str:
+        """Initializes a Unix socket connection pool for a Cloud SQL instance of Postgres."""
+        # copied from: https://cloud.google.com/sql/docs/postgres/connect-run#connect-unix-socket
+        # Note: Saving credentials in environment variables is convenient, but not
+        # secure - consider a more secure solution such as
+        # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+        # keep secrets safe.
+        db_user = values.data.get("POSTGRES_USER")  # e.g. 'my-database-user'
+        db_pass = values.data.get("POSTGRES_PASSWORD")  # e.g. 'my-database-password'
+        db_name = values.data.get("POSTGRES_DB")  # e.g. 'my-database'
+        unix_socket_path = values.data.get("INSTANCE_UNIX_SOCKET") # e.g. '/cloudsql/project:region:instance'
+
+        # Equivalent URL:
+        # postgresql+pg8000://<db_user>:<db_pass>@/<db_name>
+        #                         ?unix_sock=<INSTANCE_UNIX_SOCKET>/.s.PGSQL.5432
+        # Note: Some drivers require the `unix_sock` query parameter to use a different key.
+        # For example, 'psycopg2' uses the path set to `host` in order to connect successfully.
+        return URL.create(
+                drivername="postgresql+psycopg2",
+                username=db_user,
+                password=db_pass,
+                database=db_name,
+                query={"host": f"{unix_socket_path}/.s.PGSQL.5432"},
+            )
 
 
 settings = APISettings()
--- backend/app/database.py
+++ backend/app/database.py
@@ -1,53 +1,17 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
-from sqlalchemy.engine import Engine
 from sqlalchemy import create_engine, engine
 
 from app.config import settings
 
 
-if settings.NODE == "production":
-    # copied from: https://cloud.google.com/sql/docs/postgres/connect-run#connect-unix-socket
-
-    def connect_unix_socket() -> Engine:
-        """Initializes a Unix socket connection pool for a Cloud SQL instance of Postgres."""
-        # Note: Saving credentials in environment variables is convenient, but not
-        # secure - consider a more secure solution such as
-        # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-        # keep secrets safe.
-        db_user = settings.POSTGRES_USER  # e.g. 'my-database-user'
-        db_pass = settings.POSTGRES_PASSWORD  # e.g. 'my-database-password'
-        db_name = settings.POSTGRES_DB  # e.g. 'my-database'
-        unix_socket_path = settings.INSTANCE_UNIX_SOCKET # e.g. '/cloudsql/project:region:instance'
-
-        pool = create_engine(
-            # Equivalent URL:
-            # postgresql+pg8000://<db_user>:<db_pass>@/<db_name>
-            #                         ?unix_sock=<INSTANCE_UNIX_SOCKET>/.s.PGSQL.5432
-            # Note: Some drivers require the `unix_sock` query parameter to use a different key.
-            # For example, 'psycopg2' uses the path set to `host` in order to connect successfully.
-            engine.url.URL.create(
-                drivername="postgresql+psycopg2",
-                username=db_user,
-                password=db_pass,
-                database=db_name,
-                query={"host": f"{unix_socket_path}/.s.PGSQL.5432"},
-            ),
-            # ...
-            # connect_args={
-            # }
-        )
-        return pool
-    engine = connect_unix_socket()
-
-else:
-    engine = create_engine(
-        settings.get_db_uri_string(),
-        # connect_args={
-        #     "check_same_thread": False,
-        # }
-    )
+engine = create_engine(
+    settings.get_db_uri_string(),
+    # connect_args={
+    #     "check_same_thread": False,
+    # }
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
